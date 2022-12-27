8fa1f841f6b3bfc71591ff746277b262360f1092
Ralphvandodewaard <rtvd@ziggo.nl>
2022-12-27 16:40:59 +0100

    Added background and text colors

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,5 @@
 <template>
-  <RouterView />
+  <div class="min-h-screen p-8 bg-gray-1000 text-orange-150">
+    <RouterView />
+  </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        gray: {
+          1000: '#010B13'
+        },
+        orange: {
+          150: '#F0E6D2'
+        }
+      }
+    }
   },
   plugins: []
 };
