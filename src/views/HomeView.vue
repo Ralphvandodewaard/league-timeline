e6d0bd71b4ef93446e525f6fe0dce4d40ba8fffc
Ralphvandodewaard <rtvd@ziggo.nl>
2022-12-27 19:01:09 +0100

    Added getting random event

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,55 @@
 <template>
   <div>
-    home
+    <div
+      v-for="event in events"
+      :key="event.id"
+    >
+      {{ event.label }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, Ref, ref } from 'vue';
+import eventsList from '@/assets/events';
+import { Event } from '@/models/event';
 
 export default defineComponent({
-  name: 'HomeView'
+  name: 'HomeView',
+  setup() {
+    const events: Ref<Event[]> = ref([]);
+
+    const currentEvent: Ref<Event | null> = ref(null);
+
+    onMounted(() => {
+      getFirstEvent();
+      getCurrentEvent();
+    });
+
+    function getFirstEvent(): void {
+      events.value.push(getRandomEvent());
+    }
+
+    function getCurrentEvent(): void {
+      currentEvent.value = getRandomEvent();
+    }
+
+    function getRandomEvent(): Event {
+      let randomEvent = eventsList[getRandomNumber()];
+      while (events.value.some((event: Event) => event.id === randomEvent.id || event.date === randomEvent.date)) {
+        randomEvent = eventsList[getRandomNumber()];
+      }
+
+      return randomEvent;
+    }
+
+    function getRandomNumber(): number {
+      return Math.round(Math.random() * (eventsList.length - 1));
+    }
+
+    return {
+      events
+    };
+  }
 });
 </script>
