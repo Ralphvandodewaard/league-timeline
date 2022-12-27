66aa40801ffee8f34212d363554dc6aa50af3e35
Ralphvandodewaard <rtvd@ziggo.nl>
2022-12-27 18:00:16 +0100

    Added events list with champions up to letter J

---
+++ src/assets/events.ts
@@ -0,0 +1,313 @@
+import { Event } from '@/models/event';
+import types from './types';
+
+const events: Event[] = [
+  {
+    id: 'Aatrox',
+    label: 'Aatrox',
+    type: types.champion,
+    date: '2013-06-13'
+  },
+  {
+    id: 'Ahri',
+    label: 'Ahri',
+    type: types.champion,
+    date: '2011-12-14'
+  },
+  {
+    id: 'Akali',
+    label: 'Akali',
+    type: types.champion,
+    date: '2010-05-11'
+  },
+  {
+    id: 'Akshan',
+    label: 'Akshan',
+    type: types.champion,
+    date: '2021-07-22'
+  },
+  {
+    id: 'Alistar',
+    label: 'Alistar',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Amumu',
+    label: 'Amumu',
+    type: types.champion,
+    date: '2009-06-26'
+  },
+  {
+    id: 'Anivia',
+    label: 'Anivia',
+    type: types.champion,
+    date: '2009-07-10'
+  },
+  {
+    id: 'Annie',
+    label: 'Annie',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Aphelios',
+    label: 'Aphelios',
+    type: types.champion,
+    date: '2019-12-11'
+  },
+  {
+    id: 'Ashe',
+    label: 'Ashe',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'AurelionSol',
+    label: 'Aurelion Sol',
+    type: types.champion,
+    date: '2016-03-24'
+  },
+  {
+    id: 'Azir',
+    label: 'Azir',
+    type: types.champion,
+    date: '2014-09-16'
+  },
+  {
+    id: 'Bard',
+    label: 'Bard',
+    type: types.champion,
+    date: '2015-03-12'
+  },
+  {
+    id: 'Belveth',
+    label: "Bel'Veth",
+    type: types.champion,
+    date: '2022-06-09'
+  },
+  {
+    id: 'Blitzcrank',
+    label: 'Blitzcrank',
+    type: types.champion,
+    date: '2009-09-02'
+  },
+  {
+    id: 'Brand',
+    label: 'Brand',
+    type: types.champion,
+    date: '2011-04-12'
+  },
+  {
+    id: 'Braum',
+    label: 'Braum',
+    type: types.champion,
+    date: '2014-05-12'
+  },
+  {
+    id: 'Caitlyn',
+    label: 'Caitlyn',
+    type: types.champion,
+    date: '2011-01-04'
+  },
+  {
+    id: 'Camille',
+    label: 'Camille',
+    type: types.champion,
+    date: '2016-12-07'
+  },
+  {
+    id: 'Cassiopeia',
+    label: 'Cassiopeia',
+    type: types.champion,
+    date: '2010-12-14'
+  },
+  {
+    id: 'Chogath',
+    label: "Cho'Gath",
+    type: types.champion,
+    date: '2009-06-26'
+  },
+  {
+    id: 'Corki',
+    label: 'Corki',
+    type: types.champion,
+    date: '2009-09-19'
+  },
+  {
+    id: 'Darius',
+    label: 'Darius',
+    type: types.champion,
+    date: '2012-05-23'
+  },
+  {
+    id: 'Diana',
+    label: 'Diana',
+    type: types.champion,
+    date: '2012-08-07'
+  },
+  {
+    id: 'Draven',
+    label: 'Draven',
+    type: types.champion,
+    date: '2012-06-06'
+  },
+  {
+    id: 'DrMundo',
+    label: 'Dr. Mundo',
+    type: types.champion,
+    date: '2009-09-02'
+  },
+  {
+    id: 'Ekko',
+    label: 'Ekko',
+    type: types.champion,
+    date: '2015-05-29'
+  },
+  {
+    id: 'Elise',
+    label: 'Elise',
+    type: types.champion,
+    date: '2012-10-26'
+  },
+  {
+    id: 'Evelynn',
+    label: 'Evelynn',
+    type: types.champion,
+    date: '2009-05-01'
+  },
+  {
+    id: 'Ezreal',
+    label: 'Ezreal',
+    type: types.champion,
+    date: '2010-03-16'
+  },
+  {
+    id: 'Fiddlesticks',
+    label: 'Fiddlesticks',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Fiora',
+    label: 'Fiora',
+    type: types.champion,
+    date: '2012-02-29'
+  },
+  {
+    id: 'Fizz',
+    label: 'Fizz',
+    type: types.champion,
+    date: '2011-11-15'
+  },
+  {
+    id: 'Galio',
+    label: 'Galio',
+    type: types.champion,
+    date: '2010-08-10'
+  },
+  {
+    id: 'Gangplank',
+    label: 'Gangplank',
+    type: types.champion,
+    date: '2009-08-19'
+  },
+  {
+    id: 'Garen',
+    label: 'Garen',
+    type: types.champion,
+    date: '2010-04-27'
+  },
+  {
+    id: 'Gnar',
+    label: 'Gnar',
+    type: types.champion,
+    date: '2014-08-14'
+  },
+  {
+    id: 'Gragas',
+    label: 'Gragas',
+    type: types.champion,
+    date: '2010-02-02'
+  },
+  {
+    id: 'Graves',
+    label: 'Graves',
+    type: types.champion,
+    date: '2011-10-19'
+  },
+  {
+    id: 'Gwen',
+    label: 'Gwen',
+    type: types.champion,
+    date: '2021-04-15'
+  },
+  {
+    id: 'Hecarim',
+    label: 'Hecarim',
+    type: types.champion,
+    date: '2012-04-18'
+  },
+  {
+    id: 'Heimerdinger',
+    label: 'Heimerdinger',
+    type: types.champion,
+    date: '2009-10-10'
+  },
+  {
+    id: 'Illaoi',
+    label: 'Illaoi',
+    type: types.champion,
+    date: '2015-11-24'
+  },
+  {
+    id: 'Irelia',
+    label: 'Irelia',
+    type: types.champion,
+    date: '2010-11-16'
+  },
+  {
+    id: 'Ivern',
+    label: 'Ivern',
+    type: types.champion,
+    date: '2016-10-05'
+  },
+  {
+    id: 'Janna',
+    label: 'Janna',
+    type: types.champion,
+    date: '2009-09-02'
+  },
+  {
+    id: 'JarvanIV',
+    label: 'Jarvan IV',
+    type: types.champion,
+    date: '2011-03-01'
+  },
+  {
+    id: 'Jax',
+    label: 'Jax',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Jayce',
+    label: 'Jayce',
+    type: types.champion,
+    date: '2012-07-07'
+  },
+  {
+    id: 'Jhin',
+    label: 'Jhin',
+    type: types.champion,
+    date: '2016-02-01'
+  },
+  {
+    id: 'Jinx',
+    label: 'Jinx',
+    type: types.champion,
+    date: '2013-10-10'
+  }
+];
+
+export default events;
