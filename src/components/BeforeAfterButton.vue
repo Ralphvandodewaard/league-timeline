164efe37bf6f48cfae4cf14b5e9862312434cdf1
Ralphvandodewaard <rtvd@ziggo.nl>
2022-12-28 02:28:13 +0100

    Added border color change on before/after button hover

--- src/components/BeforeAfterButton.vue
+++ src/components/BeforeAfterButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="w-40 h-72 border-2 border-dashed border-orange-150 rounded-lg hover:animate-pulse" />
+  <button class="w-40 h-72 border-2 border-dashed border-orange-150 hover:border-blue-400 rounded-lg hover:animate-pulse duration-500" />
 </template>
 
 <script lang="ts">
