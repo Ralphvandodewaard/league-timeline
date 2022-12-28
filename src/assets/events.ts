cd7ce91907de95d8ef1236b66f6640641cbe5b4b
Ralphvandodewaard <rtvd@ziggo.nl>
2022-12-28 18:57:00 +0100

    Added champions up to letter S

--- src/assets/events.ts
+++ src/assets/events.ts
@@ -307,6 +307,444 @@ const events: Event[] = [
     label: 'Jinx',
     type: types.champion,
     date: '2013-10-10'
+  },
+  {
+    id: 'KSante',
+    label: "K'Sante",
+    type: types.champion,
+    date: '2022-11-02'
+  },
+  {
+    id: 'Kaisa',
+    label: "Kai'Sa",
+    type: types.champion,
+    date: '2018-03-07'
+  },
+  {
+    id: 'Kalista',
+    label: 'Kalista',
+    type: types.champion,
+    date: '2014-11-20'
+  },
+  {
+    id: 'Karma',
+    label: 'Karma',
+    type: types.champion,
+    date: '2011-02-01'
+  },
+  {
+    id: 'Karthus',
+    label: 'Karthus',
+    type: types.champion,
+    date: '2009-06-12'
+  },
+  {
+    id: 'Kassadin',
+    label: 'Kassadin',
+    type: types.champion,
+    date: '2009-08-07'
+  },
+  {
+    id: 'Katarina',
+    label: 'Katarina',
+    type: types.champion,
+    date: '2009-09-19'
+  },
+  {
+    id: 'Kayle',
+    label: 'Kayle',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Kayn',
+    label: 'Kayn',
+    type: types.champion,
+    date: '2017-07-12'
+  },
+  {
+    id: 'Kennen',
+    label: 'Kennen',
+    type: types.champion,
+    date: '2010-04-08'
+  },
+  {
+    id: 'Khazix',
+    label: "Kha'Zix",
+    type: types.champion,
+    date: '2012-09-27'
+  },
+  {
+    id: 'Kindred',
+    label: 'Kindred',
+    type: types.champion,
+    date: '2015-10-14'
+  },
+  {
+    id: 'Kled',
+    label: 'Kled',
+    type: types.champion,
+    date: '2016-08-10'
+  },
+  {
+    id: 'KogMaw',
+    label: "Kog'Maw",
+    type: types.champion,
+    date: '2010-06-24'
+  },
+  {
+    id: 'Leblanc',
+    label: 'LeBlanc',
+    type: types.champion,
+    date: '2010-11-02'
+  },
+  {
+    id: 'LeeSin',
+    label: 'Lee Sin',
+    type: types.champion,
+    date: '2011-04-01'
+  },
+  {
+    id: 'Leona',
+    label: 'Leona',
+    type: types.champion,
+    date: '2011-07-13'
+  },
+  {
+    id: 'Lillia',
+    label: 'Lillia',
+    type: types.champion,
+    date: '2020-07-22'
+  },
+  {
+    id: 'Lissandra',
+    label: 'Lissandra',
+    type: types.champion,
+    date: '2013-04-30'
+  },
+  {
+    id: 'Lucian',
+    label: 'Lucian',
+    type: types.champion,
+    date: '2013-08-22'
+  },
+  {
+    id: 'Lulu',
+    label: 'Lulu',
+    type: types.champion,
+    date: '2012-03-20'
+  },
+  {
+    id: 'Lux',
+    label: 'Lux',
+    type: types.champion,
+    date: '2010-10-19'
+  },
+  {
+    id: 'Malphite',
+    label: 'Malphite',
+    type: types.champion,
+    date: '2009-09-02'
+  },
+  {
+    id: 'Malzahar',
+    label: 'Malzahar',
+    type: types.champion,
+    date: '2010-06-01'
+  },
+  {
+    id: 'Maokai',
+    label: 'Maokai',
+    type: types.champion,
+    date: '2011-02-16'
+  },
+  {
+    id: 'MasterYi',
+    label: 'Master Yi',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'MissFortune',
+    label: 'Miss Fortune',
+    type: types.champion,
+    date: '2010-09-08'
+  },
+  {
+    id: 'MonkeyKing',
+    label: 'Wukong',
+    type: types.champion,
+    date: '2011-07-26'
+  },
+  {
+    id: 'Mordekaiser',
+    label: 'Mordekaiser',
+    type: types.champion,
+    date: '2010-02-24'
+  },
+  {
+    id: 'Morgana',
+    label: 'Morgana',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Nami',
+    label: 'Nami',
+    type: types.champion,
+    date: '2012-12-07'
+  },
+  {
+    id: 'Nasus',
+    label: 'Nasus',
+    type: types.champion,
+    date: '2009-10-01'
+  },
+  {
+    id: 'Nautilus',
+    label: 'Nautilus',
+    type: types.champion,
+    date: '2012-02-14'
+  },
+  {
+    id: 'Neeko',
+    label: 'Neeko',
+    type: types.champion,
+    date: '2018-12-05'
+  },
+  {
+    id: 'Nidalee',
+    label: 'Nidalee',
+    type: types.champion,
+    date: '2009-12-17'
+  },
+  {
+    id: 'Nilah',
+    label: 'Nilah',
+    type: types.champion,
+    date: '2022-07-13'
+  },
+  {
+    id: 'Nocturne',
+    label: 'Nocturne',
+    type: types.champion,
+    date: '2011-03-15'
+  },
+  {
+    id: 'Nunu',
+    label: 'Nunu',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Olaf',
+    label: 'Olaf',
+    type: types.champion,
+    date: '2010-06-09'
+  },
+  {
+    id: 'Orianna',
+    label: 'Orianna',
+    type: types.champion,
+    date: '2011-06-01'
+  },
+  {
+    id: 'Ornn',
+    label: 'Ornn',
+    type: types.champion,
+    date: '2017-08-23'
+  },
+  {
+    id: 'Pantheon',
+    label: 'Pantheon',
+    type: types.champion,
+    date: '2010-02-02'
+  },
+  {
+    id: 'Poppy',
+    label: 'Poppy',
+    type: types.champion,
+    date: '2010-01-13'
+  },
+  {
+    id: 'Pyke',
+    label: 'Pyke',
+    type: types.champion,
+    date: '2018-05-31'
+  },
+  {
+    id: 'Qiyana',
+    label: 'Qiyana',
+    type: types.champion,
+    date: '2019-06-28'
+  },
+  {
+    id: 'Quinn',
+    label: 'Quinn',
+    type: types.champion,
+    date: '2013-03-01'
+  },
+  {
+    id: 'Rakan',
+    label: 'Rakan',
+    type: types.champion,
+    date: '2017-04-19'
+  },
+  {
+    id: 'Rammus',
+    label: 'Rammus',
+    type: types.champion,
+    date: '2009-07-10'
+  },
+  {
+    id: 'RekSai',
+    label: "Rek'Sai",
+    type: types.champion,
+    date: '2014-12-11'
+  },
+  {
+    id: 'Rell',
+    label: 'Rell',
+    type: types.champion,
+    date: '2020-12-10'
+  },
+  {
+    id: 'Renata',
+    label: 'Renata Glasc',
+    type: types.champion,
+    date: '2022-02-17'
+  },
+  {
+    id: 'Renekton',
+    label: 'Renekton',
+    type: types.champion,
+    date: '2011-01-18'
+  },
+  {
+    id: 'Rengar',
+    label: 'Rengar',
+    type: types.champion,
+    date: '2012-08-21'
+  },
+  {
+    id: 'Riven',
+    label: 'Riven',
+    type: types.champion,
+    date: '2011-09-14'
+  },
+  {
+    id: 'Rumble',
+    label: 'Rumble',
+    type: types.champion,
+    date: '2011-04-26'
+  },
+  {
+    id: 'Ryze',
+    label: 'Ryze',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Samira',
+    label: 'Samira',
+    type: types.champion,
+    date: '2020-09-21'
+  },
+  {
+    id: 'Sejuani',
+    label: 'Sejuani',
+    type: types.champion,
+    date: '2012-01-17'
+  },
+  {
+    id: 'Senna',
+    label: 'Senna',
+    type: types.champion,
+    date: '2019-11-10'
+  },
+  {
+    id: 'Seraphine',
+    label: 'Seraphine',
+    type: types.champion,
+    date: '2020-10-29'
+  },
+  {
+    id: 'Sett',
+    label: 'Sett',
+    type: types.champion,
+    date: '2020-01-14'
+  },
+  {
+    id: 'Shaco',
+    label: 'Shaco',
+    type: types.champion,
+    date: '2009-10-10'
+  },
+  {
+    id: 'Shen',
+    label: 'Shen',
+    type: types.champion,
+    date: '2010-03-24'
+  },
+  {
+    id: 'Shyvana',
+    label: 'Shyvana',
+    type: types.champion,
+    date: '2011-11-01'
+  },
+  {
+    id: 'Singed',
+    label: 'Singed',
+    type: types.champion,
+    date: '2009-04-18'
+  },
+  {
+    id: 'Sion',
+    label: 'Sion',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Sivir',
+    label: 'Sivir',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Skarner',
+    label: 'Skarner',
+    type: types.champion,
+    date: '2011-08-09'
+  },
+  {
+    id: 'Sona',
+    label: 'Sona',
+    type: types.champion,
+    date: '2010-09-21'
+  },
+  {
+    id: 'Soraka',
+    label: 'Soraka',
+    type: types.champion,
+    date: '2009-02-21'
+  },
+  {
+    id: 'Swain',
+    label: 'Swain',
+    type: types.champion,
+    date: '2010-10-05'
+  },
+  {
+    id: 'Sylas',
+    label: 'Sylas',
+    type: types.champion,
+    date: '2019-01-25'
+  },
+  {
+    id: 'Syndra',
+    label: 'Syndra',
+    type: types.champion,
+    date: '2012-09-13'
   }
 ];
 
